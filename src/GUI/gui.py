dbf20912cf302404522e56a8d79232e3c6596ae7
s-hiroto12 <129835810+s-hiroto12@users.noreply.github.com>
2023-06-27 17:26:46 +0900

    add gui

--- src/GUI/gui.py
+++ src/GUI/gui.py
@@ -1,1 +1,21 @@
-import tkinter
+import PySimpleGUI as sg
+
+# レイアウトの定義
+layout = [[sg.Text('テキスト入力してください：')],
+          [sg.InputText()],
+          [sg.Button('OK'), sg.Button('キャンセル')]]
+
+# ウィンドウの作成
+window = sg.Window('テキスト入力', layout)
+
+# イベントループ
+while True:
+    event, values = window.read()
+    if event == sg.WINDOW_CLOSED or event == 'キャンセル':
+        break
+    if event == 'OK':
+        input_text = values[0]
+        sg.popup('入力されたテキスト:', input_text)
+
+# ウィンドウの破棄
+window.close()
